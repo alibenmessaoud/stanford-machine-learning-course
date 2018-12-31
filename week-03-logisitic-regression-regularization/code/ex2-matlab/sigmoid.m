ede99c88a8f25bc49aa68233ca21f1f5491ce2aa
Ali Ben Messaoud <mail2alibm@gmail.com>
2018-12-31 01:15:44 +0100

    sync

--- week-03-logisitic-regression-regularization/code/ex2-matlab/sigmoid.m
+++ week-03-logisitic-regression-regularization/code/ex2-matlab/sigmoid.m
@@ -9,10 +9,28 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
-
-
+g = 1 ./ (1 .+ exp( -1 .* z))
 
 % =============================================================
 
+
 end
+
+%{ 
+>> sigmoid(10)
+g =  0.73106
+ans =  0.73106
+>> sigmoid(10)
+g =  0.99995
+ans =  0.99995
+>> sigmoid(10000)
+g =  1
+ans =  1
+>> sigmoid(0.5)
+g =  0.62246
+ans =  0.62246
+>> sigmoid(0)
+g =  0.50000
+ans =  0.50000
+>>
+%}
